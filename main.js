2db1b098f449b4678a9e71d20a50f1559b5a1a56
vai na web <vainawebcpx@gmail.com>
2019-11-08 12:44:41 -0300

    Terminei o header - em parte, falta main e footer hoje, dia 08 novembro 2019 - sexta-feira.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function handleSearch (){
-	if (document.querySelector('.buscaInput').style.display == 'none'){  
-	document.querySelector('.buscaInput').style.display = 'flex';
+	if (document.querySelector('.headermenu-busca-Input').style.display == 'none'){  
+	document.querySelector('.headermenu-busca-Input').style.display = 'flex';
 	document.querySelector('headermenu-busca').style.border = '0.5px solid #fff';
 	} else {
-		document.querySelector('.buscaInput').style.display = 'none';
+		document.querySelector('.headermenu-busca-Input').style.display = 'none';
 		document.querySelector('headermenu-busca').style.border = 'none'
 	}
 }
