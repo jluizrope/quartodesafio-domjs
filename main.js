d44498907f31bd0914565e7359688d98b6cc3e6d
vai na web <vainawebcpx@gmail.com>
2019-11-11 13:02:53 -0300

    Recuperei o header (falta acertar detalhes) que estava quebrado. Comecei o main colocando a imagem do filme destaque (faltam detalhes), hoje, dia 11 nov 2019 segunda-feira.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function handleSearch (){
-	if (document.querySelector('.buscaInput').style.display == 'none'){  
-	document.querySelector('.buscaInput').style.display = 'flex';
-	document.querySelector('headermenu-busca').style.border = '0.5px solid #fff';
+function ativalupa (){
+	if (document.querySelector('.headermenu-bisp-buscaInput').style.display == 'none'){  
+	document.querySelector('.headermenu-bisp-buscaInput').style.display = 'flex';
+	document.querySelector('.headermenu-bisp-busca').style.border = '0.5px solid #fff';
 	} else {
-		document.querySelector('.buscaInput').style.display = 'none';
-		document.querySelector('headermenu-busca').style.border = 'none'
+		document.querySelector('.headermenu-bisp-buscaInput').style.display = 'none';
+		document.querySelector('.headermenu-bisp-busca').style.border = 'none'
 	}
 }
